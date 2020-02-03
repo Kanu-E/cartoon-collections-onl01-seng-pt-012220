e1867aadff36a9a1fde4622396bfb6f3c1f6816b
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-03 18:45:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,14 @@ def roll_call_dwarves(array)
   end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  i = 0 
+  while i < array.length 
+    new_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
@@ -18,3 +24,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
